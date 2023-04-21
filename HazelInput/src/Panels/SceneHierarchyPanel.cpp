e426439dba257b4655701720db42d20ef4313b8a
invi1998 <2267661826@qq.com>
2023-04-21 15:51:49 +0800

    属性面版绘制

--- HazelInput/src/Panels/SceneHierarchyPanel.cpp
+++ HazelInput/src/Panels/SceneHierarchyPanel.cpp
@@ -4,6 +4,7 @@
 #include "Hazel/Scene/Components.h"
 
 #include <imgui/imgui.h>
+#include <glm/gtc/type_ptr.hpp>
 
 namespace Hazel
 {
@@ -26,7 +27,23 @@ namespace Hazel
 				DrawEntityNode(entity);
 			});
 
+		// 如果在我们到达这点之前，窗口中的一个项目已经处理了我们的点击，那这里就不会重复相应
+		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered())
+		{
+			m_SelectionContext = {};
+		}
+
 		ImGui::End();
+
+		ImGui::Begin("Properties");
+
+		if (m_SelectionContext)
+		{
+			DrawComponents(m_SelectionContext);
+		}
+
+		ImGui::End();
+
 	}
 
 	void SceneHierarchyPanel::DrawEntityNode(Entity entity)
@@ -44,6 +61,32 @@ namespace Hazel
 		if (opened)
 		{
 			ImGui::TreePop();
+		}
+	}
+
+	void SceneHierarchyPanel::DrawComponents(Entity entity)
+	{
+		if (entity.HasComponent<TagComponent>())
+		{
+			auto& tag = entity.GetComponent<TagComponent>().Tag;
+
+			char buffer[256] = {};
+			strcpy_s(buffer, sizeof(buffer), tag.c_str());
+			if (ImGui::InputText("Tag", buffer, sizeof(buffer)))
+			{
+				tag = std::string(buffer);
+			}
+		}
+
+		if (entity.HasComponent<TransformComponent>())
+		{
+			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+			{
+				auto& transform = entity.GetComponent<TransformComponent>().Transform;
+				ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.1f);
+
+				ImGui::TreePop();
+			}
 		}
 	}
 }
