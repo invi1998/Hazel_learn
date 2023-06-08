78c303ba83cd37a5dbf040cb497a7efeb79a3890
invi_1998 <2267661826@qq.com>
2023-06-08 11:36:03 +0800

    overrite

--- Hazel/src/Platform/OpenGL/OpenGLUniformBuffer.h
+++ Hazel/src/Platform/OpenGL/OpenGLUniformBuffer.h
@@ -8,7 +8,7 @@ namespace Hazel {
 	{
 	public:
 		OpenGLUniformBuffer(uint32_t size, uint32_t binding);
-		virtual ~OpenGLUniformBuffer();
+		~OpenGLUniformBuffer() override;
 
 		virtual void SetData(const void* data, uint32_t size, uint32_t offset = 0) override;
 	private:
